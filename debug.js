ef5117bc5c061b64d07cda231cb9e96c02fe5088
Hube2 <Hube2@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-25 22:10:18 +0000

    see readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/blunt-ajax/tags/1.1.1@938777 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ debug.js
@@ -0,0 +1,10 @@
+  
+  /*
+  var settings = {'url':0,
+                  'callback':null,
+                  'parameters':{},
+                  'method':null,
+                  'div':null,
+                  'pass':null};
+  bluntAjax(settings);
+  */
